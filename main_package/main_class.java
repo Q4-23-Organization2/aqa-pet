189b671e3c43e406036f1bc92fc435fe64ca3322
krasstas <supremewizard81@gmail.com>
2023-12-20 16:52:48 +0200

    creating main_class root new

--- main_package/main_class.java
+++ main_package/main_class.java
@@ -2,6 +2,6 @@ package main_package;
 
 public class main_class {
     public static void main(String[] args) {
-        System.out.println("Adding main package + main_class");
+        System.out.println("Adding main package + main_class new");
     }
 }
