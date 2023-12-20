9643d46e162a5714459173d819dea02f57272b94
krasstas <85233449+krasstas@users.noreply.github.com>
2023-12-20 16:56:51 +0200

    Update master_class.java
    Changing master_class outsource

--- master_package/master_class.java
+++ master_package/master_class.java
@@ -1,5 +1,6 @@
 public class master_class {
     public static void main(String[] args) {
         System.out.println("Create master_class local");
+        System.out.println("Changing master_class outsource");
     }
 }
