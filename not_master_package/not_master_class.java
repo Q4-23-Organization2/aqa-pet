75450b2b9043b524483c01566a422b287ada5a3d
krasstas <supremewizard81@gmail.com>
2023-12-20 17:10:23 +0200

    for merge conflict

--- not_master_package/not_master_class.java
+++ not_master_package/not_master_class.java
@@ -1,7 +1,7 @@
 public class not_master_class {
     public static void main(String[] args) {
         System.out.println("not_master_package created");
-        System.out.println("not_master_package created");
-        System.out.println("not_master_package created");
+        System.out.println("dfghmeated");
+        System.out.println("ZDnm,.tymnsab");
     }
 }
