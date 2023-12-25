a4ca206618ffa6af22ce84044c60bd4bdd8d2298
krasstas <supremewizard81@gmail.com>
2023-12-25 14:28:49 +0200

    Complete all five tasks

--- main_package/HomeWork_6/Main.java
+++ main_package/HomeWork_6/Main.java
@@ -67,7 +67,7 @@ public class Main {
 
         //2.2 За допомогою оператора switch:
 
-        /*Scanner scan2 = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
         System.out.println("Chose your month by number from 1 to 12: ");
         int yourMonth2 = scan2.nextInt();
         switch (yourMonth2) {
@@ -109,7 +109,7 @@ public class Main {
                 break;
             default:
                 System.out.println("Not a valid month");
-        }*/
+        }
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
