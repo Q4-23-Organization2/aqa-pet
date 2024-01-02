34f512651386523d2b62f598c2f3bbc38a7556fb
krasstas <supremewizard81@gmail.com>
2024-01-02 18:59:30 +0200

    add <= zero check.

--- HomeWork_7/main_HW_7.java
+++ HomeWork_7/main_HW_7.java
@@ -11,30 +11,30 @@ public class main_HW_7 {
         System.out.println("Enter the length of your array: ");
         int arrayLength = input.nextInt();
         if (arrayLength <= 0) {
-      System.out.println("The array size must be greater than zero!");
-    } else {
-      int[] arrayOfNum = new int[arrayLength];
-      System.out.println("Insert your numbers: ");
-      //        Пройдёмся по всему массиву, заполняя его
-      for (int i = 0; i < arrayLength; i++) {
-        arrayOfNum[i] = input.nextInt();
-      }
-      int maxArrayNum = arrayOfNum[0];
-      for (int i = 0; i < arrayLength; i++) {
-        if (arrayOfNum[i] > maxArrayNum) {
-          maxArrayNum = arrayOfNum[i];
-        }
-      }
-      System.out.println("Max number of array is: " + maxArrayNum);
+            System.out.println("The array size must be greater than zero!");
+        } else {
+            int[] arrayOfNum = new int[arrayLength];
+            System.out.println("Insert your numbers: ");
+            //        Пройдёмся по всему массиву, заполняя его
+            for (int i = 0; i < arrayLength; i++) {
+                arrayOfNum[i] = input.nextInt();
+            }
+            int maxArrayNum = arrayOfNum[0];
+            for (int i = 0; i < arrayLength; i++) {
+                if (arrayOfNum[i] > maxArrayNum) {
+                    maxArrayNum = arrayOfNum[i];
+                }
+            }
+            System.out.println("Max number of array is: " + maxArrayNum);
 
-      int minArrayNum = arrayOfNum[0];
-      for (int i = 0; i < arrayLength; i++) {
-        if (arrayOfNum[i] < minArrayNum) {
-          minArrayNum = arrayOfNum[i];
+            int minArrayNum = arrayOfNum[0];
+            for (int i = 0; i < arrayLength; i++) {
+                if (arrayOfNum[i] < minArrayNum) {
+                    minArrayNum = arrayOfNum[i];
+                }
+            }
+            System.out.println("Min number of array is: " + minArrayNum);
         }
-      }
-      System.out.println("Min number of array is: " + minArrayNum);
-}
         System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
     }
 }
