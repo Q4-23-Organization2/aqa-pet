25fa5f2b73cdd0c2f3726eedf68693718ec24c91
krasstas <supremewizard81@gmail.com>
2024-01-03 18:13:12 +0200

    Complete task 4

--- HomeWork_7/main_HW_7.java
+++ HomeWork_7/main_HW_7.java
@@ -8,18 +8,20 @@ public class main_HW_7 {
         // 1. Написати програму, яка перевіряє чи є рядок паліндромом (використовувати масив символів - char string[].
         // Для конвертації рядка, використовувати метод toCharArray())
 
+        System.out.println("1.)++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
         // 2. Написати програму, яка знаходить мінімальне і максимальне число в масиві.
 
-        Scanner input1 = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
         System.out.println("Enter the length of your array: ");
-        int arrayLength2 = input1.nextInt();
+        int arrayLength2 = input2.nextInt();
         if (arrayLength2 <= 0) {
             System.out.println("The array size must be greater than zero!");
         } else {
             int[] arrayOfNum2 = new int[arrayLength2];
             System.out.println("Insert your numbers: ");
             for (int i = 0; i < arrayLength2; i++) {
-                arrayOfNum2[i] = input1.nextInt();
+                arrayOfNum2[i] = input2.nextInt();
             }
             int maxArrayNum = arrayOfNum2[0];
             for (int i = 0; i < arrayLength2; i++) {
@@ -37,12 +39,12 @@ public class main_HW_7 {
             }
             System.out.println("Min number of array is: " + minArrayNum);
         }
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("2.)++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 
         // 3. Написати програму, яка в масиві з випадкових цілих чисел замінює всі парні числа на 0,
         // і виводить отриманий масив на екран.
 
-        Scanner input2 = new Scanner(System.in);
+        Scanner input3 = new Scanner(System.in);
         System.out.println("Enter the length of your array: ");
         int arrayLength3 = input2.nextInt();
         if (arrayLength3 <= 0) {
@@ -51,19 +53,28 @@ public class main_HW_7 {
             int[] arrayOfNum3 = new int[arrayLength3];
             System.out.println("Insert your numbers: ");
             for (int i = 0; i < arrayLength3; i++) {
-                arrayOfNum3[i] = input2.nextInt();
+                arrayOfNum3[i] = input3.nextInt();
             }
 
-/*        int evenNumArray = arrayOfNum3[0];*/
-        for (int i = 0; i < arrayLength3; i++) {
-            if (arrayOfNum3[i] % 2 == 0) {
-                arrayOfNum3[i] = 0;
-            }
-        System.out.print("[" + arrayOfNum3[i] + "]");
+            int evenNumArray = arrayOfNum3[0];
+            for (int i = 0; i < arrayLength3; i++) {
+                if (arrayOfNum3[i] % 2 == 0) {
+                    arrayOfNum3[i] = 0;
+                }
+                System.out.print("[" + arrayOfNum3[i] + "]");
             }
             System.out.println(" Your new array");
+
+            System.out.println("3.)++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         }
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+        //4. Написати програму, яка друкує таблицю значень sin(x),
+        // де х - від 0 до 360 з кроком 10.(значення мають бути записані в масив за допомогою циклу)
+
+        double[] arraySinX = new double[360];
+        for (int i = 0; i <= arraySinX.length; i+=10) {
+            System.out.println("[sin(x)" + i + "°] = [" + Math.sin(i) + "]");
+        }
     }
 }
 
