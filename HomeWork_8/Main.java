7e38151a5b7c1a3f2b604313214a14ff517d9450
krasstas <supremewizard81@gmail.com>
2024-01-08 16:47:51 +0200

    Added users parameters.

--- HomeWork_8/Main.java
+++ HomeWork_8/Main.java
@@ -3,20 +3,47 @@ package HomeWork_8;
 public class Main {
     public static void main(String[] args) {
         User firstUser = new User();
-        firstUser.setUserName("Stanislav");
+        firstUser.userName = "Stanislav";
+        firstUser.setUserAge(42);
+        firstUser.setUserPassword("1qaz!!QQAAZZ");
 
         User secondUser = new User();
-        secondUser.setUserName("Yulia");
+        secondUser.userName = "Yulia";
+        secondUser.setUserAge(41);
+        secondUser.setUserPassword("2wsx@@WWSSXX");
 
         User thirdUser = new User();
-        thirdUser.setUserName("Igor");
+        thirdUser.userName = "Igor";
+        thirdUser.setUserAge(13);
+        thirdUser.setUserPassword("3edc##EEDDCC");
 
         User fourthUser = new User();
-        fourthUser.setUserName("Oleg");
+        fourthUser.userName = "Oleg";
+        fourthUser.setUserAge(8);
+        fourthUser.setUserPassword("4rfv$$RRFFVV");
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++");
 
         firstUser.printUserName();
+        firstUser.printUserAge();
+        firstUser.printUserPassword();
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++");
+
         secondUser.printUserName();
+        secondUser.printUserAge();
+        secondUser.printUserPassword();
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++");
+
         thirdUser.printUserName();
+        thirdUser.printUserAge();
+        thirdUser.printUserPassword();
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++");
+
         fourthUser.printUserName();
+        fourthUser.printUserAge();
+        fourthUser.printUserPassword();
     }
 }
--- HomeWork_8/User.java
+++ HomeWork_8/User.java
@@ -1,22 +1,34 @@
 package HomeWork_8;
 
 public class User {
-    private String userName;
+    public String userName;
     public String email;
     public int amountSpentMoney;
     public boolean isActive;
-    private int age;
-    private int password;
+    private int userAge;
+    private String  userPassword;
 
     public  User () {
     }
 
-    public void setUserName(String name) {
-        userName = name;
+    public void setUserAge(int age) {
+        userAge = age;
+    }
+
+    public void setUserPassword(String password) {
+        userPassword = password;
     }
 
     public void printUserName() {
         System.out.println("The user's name is: " + userName);
+    }
+
+    public void printUserAge() {
+        System.out.println("The user's age is: " + userAge);
+    }
+
+    public void printUserPassword() {
+        System.out.println("The user's password is: " + userPassword);
     }
 
     public void makePurchase() {
