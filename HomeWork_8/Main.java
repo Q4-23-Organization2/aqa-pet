b73652ba87b36d37cefd03d22b2bcba77c47c1bb
krasstas <supremewizard81@gmail.com>
2024-01-08 14:53:44 +0200

    Initial commit & create 3 classes.

---
+++ HomeWork_8/Main.java
@@ -0,0 +1,10 @@
+package HomeWork_8;
+
+public class Main {
+    public static void main(String[] args) {
+        User firstUser = new User();
+        User secondUser = new User();
+        User thirdUser = new User();
+        User fourthUser = new User();
+    }
+}
---
+++ HomeWork_8/User.java
@@ -0,0 +1,22 @@
+package HomeWork_8;
+
+public class User {
+    public String userName;
+    public String email;
+    public int amountSpentMoney;
+    public boolean isActive;
+    private int age;
+    private int password;
+
+    public  User () {
+
+    }
+
+    public void makePurchase() {
+        System.out.println("Purchase value is: ");
+    }
+
+    public void printTotalAmountOfSpentMoney() {
+        System.out.println("Purchase value is: ");
+    }
+}
