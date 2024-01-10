c88ec29a350c41b0cf0cc1d1e893cf2a55ca309b
krasstas <supremewizard81@gmail.com>
2024-01-10 19:55:27 +0200

    Add static doubles to User Calculator.

--- HomeWork_8/Calculator.java
+++ HomeWork_8/Calculator.java
@@ -1,8 +1,19 @@
 package HomeWork_8;
 
 public class Calculator {
-    public static double additionOperation;
-    public static double subtractionOperation;
-    public static double multiplicationOperation;
-    public static double divisionOperation;
+    public static double a = 56.923;
+    public static double b = 14.22;
+
+    public static void addition () {
+        System.out.println("The sum of the addition is: " + (a + b));
+    }
+    public static void subtraction () {
+        System.out.println("The sum of the addition is: " + (a - b));
+    }
+    public static void multiplicationOperation () {
+        System.out.println("The sum of the addition is: " + (a * b));
+    }
+    public static void divisionOperation () {
+        System.out.println("The sum of the addition is: " + (a / b));
+    }
 }
--- HomeWork_8/Main_Calc.java
+++ HomeWork_8/Main_Calc.java
@@ -2,6 +2,8 @@ package HomeWork_8;
 
 public class Main_Calc {
     public static void main(String[] args) {
-
+        Main_Calc additionOperation = new Main_Calc();
+        additionOperation.addition();
     }
+
 }
