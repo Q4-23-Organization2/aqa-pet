4c2a0ad1146da322ef7ff24315bfcea6d93fcf6d
krasstas <supremewizard81@gmail.com>
2024-01-13 20:30:16 +0200

    Adding standard functions to calculators family.

--- HomeWork_9/Calculators.java
+++ HomeWork_9/Calculators.java
@@ -1,7 +1,22 @@
 package HomeWork_9;
 
 public class Calculators {
-    public static void main(String[] args) {
+
+    public static double additionOperation(double firstOperand, double secondOperand) {
+        return firstOperand + secondOperand;
+    }
+
+    public static double subtractionOperation(double firstOperand, double secondOperand) {
+        return firstOperand - secondOperand;
+    }
+
+    public static double multiplicationOperation(double firstOperand, double secondOperand) {
+        return firstOperand * secondOperand;
+
+    }
+
+    public static double divisionOperation(double firstOperand, double secondOperand) {
+        return firstOperand / secondOperand;
 
     }
 }
--- HomeWork_9/ScientificCalculator.java
+++ HomeWork_9/ScientificCalculator.java
@@ -2,6 +2,9 @@ package HomeWork_9;
 
 public class ScientificCalculator {
     public static void main(String[] args) {
-
+        System.out.println(Calculators.additionOperation(56.88965, 12.55157));
+        System.out.println(Calculators.subtractionOperation(56.88965, 12.55157));
+        System.out.println(Calculators.multiplicationOperation(56.88965, 12.55157));
+        System.out.println(Calculators.divisionOperation(56.88965, 12.55157));
     }
 }
