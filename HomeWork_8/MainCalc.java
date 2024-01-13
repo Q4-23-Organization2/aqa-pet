3cf6205c14d087ce7d984b9f54f5da457dcbcfb6
krasstas <supremewizard81@gmail.com>
2024-01-13 13:19:22 +0200

    Optimise constructors.

--- HomeWork_8/MainCalc.java
+++ HomeWork_8/MainCalc.java
@@ -3,7 +3,6 @@ import static HomeWork_8.Calculator.additionOperation;
 import static HomeWork_8.Calculator.subtractionOperation;
 public class MainCalc {
     public static void main(String[] args) {
-/*        Calculator operand = new Calculator(45.893, 23.127);*/
         additionOperation();
         subtractionOperation();
         Calculator.multiplicationOperation();
