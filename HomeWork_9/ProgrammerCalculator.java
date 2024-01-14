cbaa74b831a523fd9773a15aebfbbbf6f8fa284b
krasstas <supremewizard81@gmail.com>
2024-01-14 17:22:57 +0200

    Adding special functions to children calculators.

--- HomeWork_9/ProgrammerCalculator.java
+++ HomeWork_9/ProgrammerCalculator.java
@@ -2,7 +2,7 @@ package HomeWork_9;
 
 public class ProgrammerCalculator extends Calculators {
     public static String programmerCalculation(int binary) {
-        return Integer.toString(binary, 2); }
+        return Integer.toString(binary, 2); } //Перевод в двоичную симтему исчисления.
     public static void main(String[] args) {
         System.out.println(Calculators.additionOperation(56.88965, 12.55157));
         System.out.println(Calculators.subtractionOperation(56.88965, 12.55157));
--- HomeWork_9/ScientificCalculator.java
+++ HomeWork_9/ScientificCalculator.java
@@ -2,7 +2,7 @@ package HomeWork_9;
 
 public class ScientificCalculator extends Calculators {
     private static double scientificCalculation() {
-        return Math.sqrt(25.0); }
+        return Math.sqrt(25.0); } //Извлечение квадратного корня.
     public static void main(String[] args) {
         System.out.println(Calculators.additionOperation(56.88965, 12.55157));
         System.out.println(Calculators.subtractionOperation(56.88965, 12.55157));
