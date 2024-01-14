b3573847db1ba0b6ae3990beada4b4c815eafb62
krasstas <supremewizard81@gmail.com>
2024-01-14 17:41:24 +0200

    Adding special functions to children calculators.

--- HomeWork_9/ProgrammerCalculator.java
+++ HomeWork_9/ProgrammerCalculator.java
@@ -4,10 +4,10 @@ public class ProgrammerCalculator extends Calculators {
     public static String programmerCalculation(int binary) {
         return Integer.toString(binary, 2); } //Перевод в двоичную симтему исчисления.
     public static void main(String[] args) {
-        System.out.println(Calculators.additionOperation(56.88965, 12.55157));
-        System.out.println(Calculators.subtractionOperation(56.88965, 12.55157));
-        System.out.println(Calculators.multiplicationOperation(56.88965, 12.55157));
-        System.out.println(Calculators.divisionOperation(56.88965, 12.55157));
+        System.out.println(additionOperation(56.88965, 12.55157));
+        System.out.println(subtractionOperation(56.88965, 12.55157));
+        System.out.println(multiplicationOperation(56.88965, 12.55157));
+        System.out.println(divisionOperation(56.88965, 12.55157));
         System.out.println(programmerCalculation(1024));
     }
 }
--- HomeWork_9/ScientificCalculator.java
+++ HomeWork_9/ScientificCalculator.java
@@ -4,10 +4,10 @@ public class ScientificCalculator extends Calculators {
     private static double scientificCalculation() {
         return Math.sqrt(25.0); } //Извлечение квадратного корня.
     public static void main(String[] args) {
-        System.out.println(Calculators.additionOperation(56.88965, 12.55157));
-        System.out.println(Calculators.subtractionOperation(56.88965, 12.55157));
-        System.out.println(Calculators.multiplicationOperation(56.88965, 12.55157));
-        System.out.println(Calculators.divisionOperation(56.88965, 12.55157));
+        System.out.println(additionOperation(56.88965, 12.55157));
+        System.out.println(subtractionOperation(56.88965, 12.55157));
+        System.out.println(multiplicationOperation(56.88965, 12.55157));
+        System.out.println(divisionOperation(56.88965, 12.55157));
         System.out.println(scientificCalculation());
     }
 }
