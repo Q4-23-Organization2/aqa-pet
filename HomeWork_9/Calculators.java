9b88142a110d0e1c1100b4c0b4f189aba082dde3
krasstas <supremewizard81@gmail.com>
2024-01-14 17:54:46 +0200

    Adding special functions to children calculators.

--- HomeWork_9/Calculators.java
+++ HomeWork_9/Calculators.java
@@ -1,17 +1,17 @@
 package HomeWork_9;
 
 public class Calculators {
-    protected static double additionOperation(double firstOperand, double secondOperand) {
+    public static double additionOperation(double firstOperand, double secondOperand) {
         return firstOperand + secondOperand;
     }
-    protected static double subtractionOperation(double firstOperand, double secondOperand) {
+    public static double subtractionOperation(double firstOperand, double secondOperand) {
         return firstOperand - secondOperand;
     }
-    protected static double multiplicationOperation(double firstOperand, double secondOperand) {
+    public static double multiplicationOperation(double firstOperand, double secondOperand) {
         return firstOperand * secondOperand;
 
     }
-    protected static double divisionOperation(double firstOperand, double secondOperand) {
+    public static double divisionOperation(double firstOperand, double secondOperand) {
         return firstOperand / secondOperand;
     }
 }
