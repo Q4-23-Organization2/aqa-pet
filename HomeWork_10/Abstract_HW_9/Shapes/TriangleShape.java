12ca7247fe39e059348c2852d425baf9fe762013
krasstas <supremewizard81@gmail.com>
2024-01-23 21:01:26 +0200

    Restructure TriangleShape to abstract class.

---
+++ HomeWork_10/Abstract_HW_9/Shapes/TriangleShape.java
@@ -0,0 +1,25 @@
+package HomeWork_10.Abstract_HW_9.Shapes;
+
+class TriangleShape extends Shapes {
+    private final double sideA;
+    private final double sideB;
+    private final double sideC;
+
+    public TriangleShape(String color, double sideA, double sideB, double sideC) {
+        super(color);
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return sideA + sideB + sideC;
+    }
+
+    @Override
+    public double calculateArea() {
+        double s = (sideA + sideB + sideC) / 2;
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+    }
+}
