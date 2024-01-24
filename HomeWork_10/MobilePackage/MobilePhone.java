cbc146fd2edb80a0e82262146b49cfc4305d5033
krasstas <supremewizard81@gmail.com>
2024-01-24 12:28:00 +0200

    Adding abstract method to MobilePhone class. Overriding parents methods.

--- HomeWork_10/MobilePackage/MobilePhone.java
+++ HomeWork_10/MobilePackage/MobilePhone.java
@@ -1,4 +1,25 @@
 package HomeWork_10.MobilePackage;
 
-public class MobilePhone {
+abstract class MobilePhone implements Mobile {
+    private final String mobileBrand;
+
+    MobilePhone(String mobileBrand) {
+        this.mobileBrand = mobileBrand;
+    }
+    public String getMobileBrand () {
+
+        return getMobileBrand();
+    }
+
+    public abstract void makeScreenShot();
+
+    @Override
+    public void makeCall(int phoneNumber) {
+        System.out.println(mobileBrand + " calls the number: " + phoneNumber);
+    }
+
+    @Override
+    public void sendSMS(int phoneNumber, String textMessage) {
+        System.out.println(mobileBrand + " sending SMS to: " + phoneNumber + " . Message text is: " + textMessage);
+    }
 }
