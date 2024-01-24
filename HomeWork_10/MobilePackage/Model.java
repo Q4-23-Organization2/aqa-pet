fd4abba4ae85c7708c183ca1fd3dbd055db838bf
krasstas <supremewizard81@gmail.com>
2024-01-24 12:41:39 +0200

    Adding own method to Model class.

--- HomeWork_10/MobilePackage/Model.java
+++ HomeWork_10/MobilePackage/Model.java
@@ -1,4 +1,19 @@
 package HomeWork_10.MobilePackage;
 
-public class Model {
+public class Model extends MobilePhone{
+    private final String phoneModel;
+
+    Model(String mobileBrand, String phoneModel) {
+        super(mobileBrand);
+        this.phoneModel = phoneModel;
+    }
+
+    @Override
+    public void makeScreenShot() {
+        System.out.println(phoneModel + "im making a screenshot in application.");
+    }
+
+    public void phoneCharge () {
+        System.out.println(phoneModel + "is charging now.");
+    }
 }
