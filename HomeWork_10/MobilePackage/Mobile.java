41ab7d7647a508151fd2769eab8f7dce03d341b2
krasstas <supremewizard81@gmail.com>
2024-01-24 13:26:50 +0200

    Complete task "Mobile".

--- HomeWork_10/MobilePackage/Mobile.java
+++ HomeWork_10/MobilePackage/Mobile.java
@@ -1,7 +1,7 @@
 package HomeWork_10.MobilePackage;
 
 interface Mobile {
-    final String RECHARGE = "Don't forget to charge your mobile phone periodically!";
+    String RECHARGE = "Don't forget to charge your mobile phone periodically!";
     void makeCall(String phoneNumber);
     void sendSMS(String phoneNumber, String textMessage);
 }
