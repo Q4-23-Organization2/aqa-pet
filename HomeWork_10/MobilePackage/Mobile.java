5cd407b10ab565bfd272d4cb66299df6093a4704
krasstas <supremewizard81@gmail.com>
2024-01-24 12:13:20 +0200

    Description & definition of the Mobile interface.

--- HomeWork_10/MobilePackage/Mobile.java
+++ HomeWork_10/MobilePackage/Mobile.java
@@ -1,6 +1,7 @@
 package HomeWork_10.MobilePackage;
 
 interface Mobile {
+    static final String RECHARGE = "Don't forget to charge your mobile phone periodically!";
     void makeCall(int phoneNumber);
     void sendSMS(int phoneNumber, String textMessage);
 }
