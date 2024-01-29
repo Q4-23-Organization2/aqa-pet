482b8ae30d96e01fea8f5ffa80eebb5959497c0c
krasstas <85233449+krasstas@users.noreply.github.com>
2024-01-29 14:36:29 +0200

    Home work 9 (#13)
    * final commit before merge request
    
    * final commit before merge request
    
    * final commit before merge request
    
    * Configure .gitignore.
    
    * Initial commit. Create SuperClass + SubClasses.
    
    * Create SuperClass + SubClasses Calculators. Create Main classes.
    
    * Adding standard functions to calculators family.
    
    * Extends children calculators.
    
    * Adding special functions to children calculators.
    
    * Adding special functions to children calculators.
    
    * Adding special functions to children calculators.
    
    * Adding special functions to children calculators.
    
    * Adding special functions to children calculators.
    
    * Adding special functions to children calculators.
    
    * Adding special functions to children calculators.
    
    * Adding special functions to children calculators.
    
    * Попытки решить первую задачу.
    
    * Adding RectangleShape
    
    * Refactoring & reconfigure classes.
    
    * Description & definition of the Circle class.
    
    * Description & definition of the Rectangle class.
    
    * Description & definition of the Triangle class.
    
    * Description & definition of the Main_Shapes class.
    
    * Configure .gitignore.
    
    * Recreate final fields.

---
+++ HomeWork_9/Shapes/CircleShape.java
@@ -0,0 +1,42 @@
+package HomeWork_9.Shapes;
+
+import java.util.Objects;
+
+public class CircleShape extends Shapes {
+    private final double circleRadius;
+
+    public CircleShape(String color, double circleRadius) {
+        super(color);
+        this.circleRadius = circleRadius;
+    }
+
+    @Override
+    protected double calculatePerimeter() {
+        return 2 * Math.PI * circleRadius;
+    }
+
+    @Override
+    protected double calculateArea() {
+        return Math.PI * circleRadius * circleRadius;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CircleShape that)) return false;
+        if (!super.equals(o)) return false;
+        return Double.compare(circleRadius, that.circleRadius) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), circleRadius);
+    }
+
+    @Override
+    public String toString() {
+        return "CircleShape{" +
+                "circleRadius=" + circleRadius +
+                "} " + super.toString();
+    }
+}
---
+++ HomeWork_9/Shapes/TriangleShape.java
@@ -0,0 +1,51 @@
+package HomeWork_9.Shapes;
+
+import java.util.Objects;
+
+public class TriangleShape extends Shapes {
+    private final double sideA;
+    private final double sideB;
+    private final double sideC;
+
+    public TriangleShape(String color, double sideA, double sideB, double sideC) {
+        super(color);
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    @Override
+    protected double calculatePerimeter() {
+        return sideA + sideB + sideC;
+    }
+
+    @Override
+    protected double calculateArea() {
+        //Calculate the area of the triangle using the formula
+        double halfPerimeter = (sideA + sideB + sideC) / 2;
+        return Math.sqrt(halfPerimeter * (halfPerimeter - sideA) * (halfPerimeter - sideB) * (halfPerimeter - sideC));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TriangleShape that)) return false;
+        if (!super.equals(o)) return false;
+        return Double.compare(sideA, that.sideA) == 0 && Double.compare(sideB, that.sideB) == 0 && Double.compare(sideC, that.sideC) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), sideA, sideB, sideC);
+    }
+
+    @Override
+    public String toString() {
+        return "TriangleShape{" +
+                "sideA=" + sideA +
+                ", sideB=" + sideB +
+                ", sideC=" + sideC +
+                "} " + super.toString();
+    }
+}
+
