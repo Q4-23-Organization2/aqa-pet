cf0e1b3566d92baa5887c87f3a31ffb6e1aefcbb
krasstas <supremewizard81@gmail.com>
2024-01-29 12:47:01 +0200

    Adding scanner for chooses.

--- HomeWork_11/RPSGame.java
+++ HomeWork_11/RPSGame.java
@@ -1,12 +1,25 @@
 package HomeWork_11;
 
+import java.util.Scanner;
+
 abstract class RPSGame {
     private final int MIN_COUNT_PLAYERS = 2;
     private final int MAX_COUNT_PLAYERS = 6;
+    private GamePlayer[] gamePlayers;
 
     public RPSGame(int playersCount) {
         if (playersCount < MIN_COUNT_PLAYERS || playersCount > MAX_COUNT_PLAYERS) {
             throw new IllegalArgumentException("Invalid number of players entered!");
         }
+
+        gamePlayers = new GamePlayer[playersCount];
+        for (int i = 0; i < playersCount; i++) {
+            gamePlayers[i] = new GamePlayer(i + 1);
+        }
+    }
+
+    public void playGame() {
+        Scanner scanner = new Scanner()
     }
 }
+
