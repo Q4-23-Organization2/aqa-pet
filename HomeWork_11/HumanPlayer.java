3fb4f2123cb63941d4f55150ac9976fa8ba1164d
krasstas <supremewizard81@gmail.com>
2024-01-31 17:03:13 +0200

    Adding class "HumanPlayer".

---
+++ HomeWork_11/HumanPlayer.java
@@ -0,0 +1,24 @@
+package HomeWork_11;
+
+import java.util.Scanner;
+
+public class HumanPlayer extends GamePlayer{
+
+    public HumanPlayer(String playerName) {
+        super(playerName);
+    }
+
+    // Метод для выбора предмета
+    public void makeChoice(Scanner scanner) {
+        System.out.println(playerName + ", введите ваш выбор (ROCK, PAPER, SCISSORS):");
+        String input = scanner.nextLine().toUpperCase();
+
+        try {
+            choice = GameItems.valueOf(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Неправильный выбор. Пожалуйста, выберите ROCK, PAPER или SCISSORS.");
+            // Рекурсивный вызов метода makeChoice, пока не будет сделан правильный выбор
+            makeChoice(scanner);
+        }
+    }
+}
