8d49a329451c7d96d1f7af920109e46a1b6d0472
krasstas <supremewizard81@gmail.com>
2024-02-04 13:18:58 +0200

    Create random list.

--- HomeWork_12/DuplicatesList.java
+++ HomeWork_12/DuplicatesList.java
@@ -1,7 +1,21 @@
 package HomeWork_12;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class DuplicatesList {
-    public static List<Integer>
+    public static List<Integer> randomList(int listSize, int minNum, int maxNum) {
+        List<Integer> randomListNumbers = new ArrayList<>();
+        Random randomNumbers = new Random();
+        for (int i = 0; i < listSize; i++) {
+            randomListNumbers.add(randomNumbers.nextInt(maxNum - minNum +1) + minNum);
+        }
+        return randomListNumbers;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(randomList(100, -20, 20));
+    }
 }
+
