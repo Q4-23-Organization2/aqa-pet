d6f42931c4c5023c1ffdf5f59a14eee2ebeada20
krasstas <supremewizard81@gmail.com>
2024-02-04 18:42:19 +0200

    Create Class UnsortedWordsList. Adding list of words.

---
+++ HomeWork_12/UnsortedList/UnsortedWordsList.java
@@ -0,0 +1,33 @@
+package HomeWork_12.UnsortedList;
+
+import java.util.*;
+
+public class UnsortedWordsList {
+
+    public static void main(String[] args) {
+
+        // Создаем список слов
+        List<String> words = new ArrayList<>();
+        words.add("C++");
+        words.add("C#");
+        words.add("Pascal");
+        words.add("pascal");
+        words.add("VBScript");
+        words.add("vbscript");
+        words.add("Java");
+        words.add("java");
+        words.add("JavaScript");
+        words.add("javascript");
+        words.add("Python");
+        words.add("python");
+        words.add("PHP");
+        words.add("php");
+        words.add("Swift");
+        words.add("swift");
+        words.add("ruby");
+        words.add("Ruby");
+        words.add("TypeScript");
+        words.add("typescript");
+
+
+}
