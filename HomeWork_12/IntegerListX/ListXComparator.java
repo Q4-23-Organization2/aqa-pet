2f41eb3ce3075072c3ed2e96c2552fe661a4192b
krasstas <supremewizard81@gmail.com>
2024-02-04 20:04:52 +0200

    Create class 'ListXComparator'.

---
+++ HomeWork_12/IntegerListX/ListXComparator.java
@@ -0,0 +1,18 @@
+package HomeWork_12.IntegerListX;
+
+import java.util.Comparator;
+
+class ListXComparator implements Comparator<Integer> {
+    private int x;
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return 0;
+    }
+
+    public ListXComparator(int x) {
+        this.x = x;
+
+
+    }
+}
