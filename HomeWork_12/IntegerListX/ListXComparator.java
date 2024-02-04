bff8c23fbc30cb6842d3a8d9ae49391211bf8f01
krasstas <supremewizard81@gmail.com>
2024-02-04 20:06:23 +0200

    Create @Override public int compare.

--- HomeWork_12/IntegerListX/ListXComparator.java
+++ HomeWork_12/IntegerListX/ListXComparator.java
@@ -5,6 +5,11 @@ import java.util.Comparator;
 class ListXComparator implements Comparator<Integer> {
     private int x;
 
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return 0;
+    }
+
     public ListXComparator(int x) {
         this.x = x;
 
