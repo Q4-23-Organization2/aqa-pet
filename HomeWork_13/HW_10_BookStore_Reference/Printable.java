5bdd9e22e9580f40cbbef888909b025de57a9ec1
krasstas <supremewizard81@gmail.com>
2024-02-07 18:39:13 +0200

    Initial commit. Create interface.

---
+++ HomeWork_13/HW_10_BookStore_Reference/Printable.java
@@ -0,0 +1,14 @@
+package HomeWork_13.HW_10_BookStore_Reference;
+
+public interface Printable {
+    // Объявление метода print, который должны реализовать классы, реализующие интерфейс Printable
+    void print();
+
+    // Дефолтный метод для печати массива объектов типа Printable
+    default void printArray(Printable[] prints) {
+        // Итерация по массиву и вызов метода print() для каждого элемента
+        for (Printable printable : prints) {
+            printable.print();
+        }
+    }
+}
