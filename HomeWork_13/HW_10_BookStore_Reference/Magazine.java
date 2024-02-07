303ef2eb40407e9d0ded7ea84b8b7f1a132733e9
krasstas <supremewizard81@gmail.com>
2024-02-07 18:47:00 +0200

    Create class 'Magazine'.

---
+++ HomeWork_13/HW_10_BookStore_Reference/Magazine.java
@@ -0,0 +1,17 @@
+package HomeWork_13.HW_10_BookStore_Reference;
+
+// Класс Magazine, реализующий интерфейс Printable
+class Magazine implements Printable {
+    private final String magazineTitle;
+
+    // Конструктор класса Magazine
+    public Magazine(String magazineTitle) {
+        this.magazineTitle = magazineTitle;
+    }
+
+    // Реализация метода print() для класса Magazine
+    @Override
+    public void print() {
+        System.out.println("The magazine title is: " + magazineTitle);
+    }
+}
