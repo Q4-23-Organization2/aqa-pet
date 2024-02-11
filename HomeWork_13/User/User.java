f977bdb277114cacedd19ad76603b82d7e4616c1
krasstas <supremewizard81@gmail.com>
2024-02-11 17:19:28 +0200

    Create constructors, getters in class 'User'.

--- HomeWork_13/User/User.java
+++ HomeWork_13/User/User.java
@@ -5,5 +5,23 @@ class User {
     private String secondName;
     private int age;
 
+    // Конструктор для создания объекта пользователя
+    public User(String firstName, String secondName, int age) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+    }
 
+    // Геттеры для получения значений полей
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
