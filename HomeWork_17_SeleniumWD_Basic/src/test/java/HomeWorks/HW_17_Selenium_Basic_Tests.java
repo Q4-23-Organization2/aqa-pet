86d9e667920ee63c76000a4745eb75a47c94dc5e
krasstas <supremewizard81@gmail.com>
2024-03-15 12:43:04 +0200

    Add @Test.

--- HomeWork_17_SeleniumWD_Basic/src/test/java/HomeWorks/HW_17_Selenium_Basic_Tests.java
+++ HomeWork_17_SeleniumWD_Basic/src/test/java/HomeWorks/HW_17_Selenium_Basic_Tests.java
@@ -1,6 +1,12 @@
 package HomeWorks;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
 
 public class HW_17_Selenium_Basic_Tests {
 
