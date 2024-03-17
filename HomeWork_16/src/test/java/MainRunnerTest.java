a91ba13a2dd71c3715c8134f7443edc7a783ec06
krasstas <supremewizard81@gmail.com>
2024-03-17 20:29:16 +0200

    Reconfigure tests params. Adding logs for tests.

--- HomeWork_16/src/test/java/MainRunnerTest.java
+++ HomeWork_16/src/test/java/MainRunnerTest.java
@@ -1,6 +1,5 @@
 import HW_13_Java_8_Separate_Methods.MainRunner;
 import HW_13_Java_8_Separate_Methods.User;
-import jdk.jfr.Description;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
@@ -28,8 +27,7 @@ public class MainRunnerTest {
     }
 
     // Положительные тесты
-    @Test
-    @Description("Checking the first and last user for age after sorting")
+    @Test(description = "Checking the first and last user for age after sorting")
     public void testSortUsersByAgeFirstCase() {
         logger.info("Running testSortUsersByAge...");
         MainRunner.sortUsersByAge(users);
@@ -41,8 +39,7 @@ public class MainRunnerTest {
         logger.info("The last user is the oldest");
     }
 
-    @Test
-    @Description("Checking each user from the list for age after sorting with an interval of 5 years")
+    @Test(description = "Checking each user from the list for age after sorting with an interval of 5 years")
     public void testSortUsersByAgeSecondCase() {
         logger.info("Running testSortUsersByAge...");
         MainRunner.sortUsersByAge(users);
@@ -51,21 +48,19 @@ public class MainRunnerTest {
         for (int i = 0; i < users.size(); i++) {
             User user = users.get(i);
             int expectedAge = i * 5 + 15; // Возраст должен увеличиваться на 5 с каждым следующим пользователем
-            Assert.assertEquals(user.getAge(), expectedAge);
+            Assert.assertEquals(user.getAge(), expectedAge, "Each subsequent user is 5 years older than the previous one");
         }
         logger.info("Each subsequent user is 5 years older than the previous one");
     }
 
-    @Test
-    @Description("Calculate the average age of all users")
+    @Test(description = "Calculate the average age of all users")
     public void testCalculateAverageAge() {
         logger.info("Running testCalculateAverageAge...");
         double averageAge = MainRunner.calculateAverageAge(users);
         Assert.assertEquals(averageAge, 37.5);
     }
 
-    @Test
-    @Description("Checking that the total age is less than 400 years")
+    @Test(description = "Checking that the total age is less than 400 years")
     public void testTotalAgeLessThan400() {
         logger.info("Running testTotalAgeLessThan400...");
         int totalAge = 0;
@@ -78,8 +73,7 @@ public class MainRunnerTest {
         logger.info("The total age of all users is less than 400 years");
     }
 
-    @Test(dataProvider = "firstUserParams")
-    @Description("Parameterized test with check for the first user of a sorted list")
+    @Test(dataProvider = "firstUserParams", description = "Parameterized test with check for the first user of a sorted list")
     public void testSortUsersByNameAndAge_FirstUser(String expectedFirstName, int expectedAge) {
         logger.info("Running testSortUsersByNameAndAge_FirstUser...");
         // Создаем копию списка пользователей, чтобы избежать изменения исходного списка
@@ -100,9 +94,8 @@ public class MainRunnerTest {
         };
     }
 
-    @Test(dataProvider = "seventhUserParams")
-    @Description("Parameterized test with check for the seventh user of a sorted list")
-    public void testSortUsersByNameAndAge_SevenUser(String expectedFirstName, int expectedAge) {
+    @Test(dataProvider = "seventhUserParams", description = "Parameterized test with check for the seventh user of a sorted list")
+    public void testSortUsersByNameAndAge_SeventhUser(String expectedFirstName, int expectedAge) {
         logger.info("Running testSortUsersByNameAndAge_MiddleUser...");
         // Создаем копию списка пользователей, чтобы избежать изменения исходного списка
         List<User> sortedUsers = new ArrayList<>(users);
@@ -119,20 +112,27 @@ public class MainRunnerTest {
                 + seventhUser.getFirstName() + " " + seventhUser.getSecondName()  + ", age: " + seventhUser.getAge());
     }
 
-
     @DataProvider(name = "seventhUserParams")
-    public Object[][] getMiddleUserParams() {
+    public Object[][] getSeventhUserParams() {
         return new Object[][]{
                 {"Keanu", 15} // Ожидаемое имя и возраст пользователя в середине списка после сортировки
         };
     }
 
-    // Отрицательный тест
-    @Test(dataProvider = "negativeTestData")
+    // Негативный тест на проверку пользователей, у которых фамилия начинается с буквы "S" или "А",
+    // с возвращением пустого листа
+    @Test(dataProvider = "negativeTestData", description = "Negative test for checking users, returning an empty list")
     public void testCheckUsersWithSorA(List<User> users) {
         logger.info("Running testCheckUsersWithSorA...");
-        List<User> result = MainRunner.checkUsersWithSorA(users);
-        Assert.assertTrue(result.isEmpty());
+        try {
+            List<User> result = MainRunner.checkUsersWithSorA(users);
+            // Проверяем, что результат пустой (список пользователей с фамилией, начинающейся на 'S' или 'A')
+            Assert.assertTrue(result.isEmpty());
+        } catch (NullPointerException e) {
+            // Если было сгенерировано исключение NullPointerException, возвращаем пустой список
+            logger.error("NullPointerException occurred: " + e.getMessage());
+            Assert.fail("NullPointerException occurred: " + e.getMessage());
+        }
     }
 
     @DataProvider(name = "negativeTestData")
@@ -143,10 +143,22 @@ public class MainRunnerTest {
         };
     }
 
-    @Test
-    public void testCheckAllUsersOlderThan18() {
-        logger.info("Running testCheckAllUsersOlderThan18...");
+    @Test(description = "Checking that NOT all users elder than 18 years")
+    public void testCheckNotAllUsersOlderThan18() {
+        List<User> users = MainRunner.createUserList(); // Получаем список пользователей
+        logger.info("Running testCheckNotAllUsersOlderThan18...");
         boolean allOlderThan18 = MainRunner.checkAllUsersOlderThan18(users);
-        Assert.assertFalse(allOlderThan18);
+        // Проверяем, все ли пользователи старше 18 лет
+
+        // Если не все пользователи старше 18 лет, найдем пользователя младше 18 лет и выведем его информацию
+        if (!allOlderThan18) {
+            for (User user : users) {
+                if (user.getAge() < 18) {
+                    logger.info("Younger than 18 years user is: " + user.getFirstName() + " "
+                            + user.getSecondName() + ", age: " + user.getAge());
+                    break; // Завершаем цикл после нахождения пользователя младше 18 лет
+                }
+            }
+        }
     }
 }
