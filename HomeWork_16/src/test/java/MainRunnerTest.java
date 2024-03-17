e1266b661f727a1603faff7f95e7649f327beac5
krasstas <supremewizard81@gmail.com>
2024-03-17 15:38:40 +0200

    Reconfigure tests.

--- HomeWork_16/src/test/java/MainRunnerTest.java
+++ HomeWork_16/src/test/java/MainRunnerTest.java
@@ -1,5 +1,6 @@
 import HW_13_Java_8_Separate_Methods.MainRunner;
 import HW_13_Java_8_Separate_Methods.User;
+import jdk.jfr.Description;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
@@ -27,24 +28,36 @@ public class MainRunnerTest {
 
     // Положительные тесты
     @Test
-    public void testSortUsersByAge() {
+    @Description("Checking the first and last user for age after sorting")
+    public void testSortUsersByAgeFirstCase() {
         logger.info("Running testSortUsersByAge...");
         MainRunner.sortUsersByAge(users);
+        // Проверяем первый элемент в отсортированном списке
         Assert.assertEquals(users.get(0).getAge(), 15);
+        // Проверяем последний элемент в отсортированном списке
+        Assert.assertEquals(users.get(users.size() - 1).getAge(), 60);
     }
 
     @Test
+    @Description("Checking each user from the list for age after sorting with an interval of 5 years")
     public void testSortUsersByAgeSecondCase() {
-        logger.info("Running testSortUsersByAgeSecondCase...");
+        logger.info("Running testSortUsersByAge...");
         MainRunner.sortUsersByAge(users);
-        Assert.assertEquals(users.get(9).getAge(), 50);
+
+        // Проверяем возраст каждого пользователя после сортировки
+        for (int i = 0; i < users.size(); i++) {
+            User user = users.get(i);
+            int expectedAge = i * 5 + 15; // Возраст должен увеличиваться на 5 с каждым следующим пользователем
+            Assert.assertEquals(user.getAge(), expectedAge);
+        }
     }
 
     @Test
+    @Description("Calculate the average age of all users")
     public void testCalculateAverageAge() {
         logger.info("Running testCalculateAverageAge...");
         double averageAge = MainRunner.calculateAverageAge(users);
-        Assert.assertEquals(averageAge, 29.2);
+        Assert.assertEquals(averageAge, 37.5);
     }
 
     @Test
