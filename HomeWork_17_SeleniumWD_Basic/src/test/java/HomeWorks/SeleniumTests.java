0986173774bf3704bbedd51c320c8b9818f24cac
krasstas <supremewizard81@gmail.com>
2024-03-19 20:31:51 +0200

    Configure first UI test.

--- HomeWork_17_SeleniumWD_Basic/src/test/java/HomeWorks/SeleniumTests.java
+++ HomeWork_17_SeleniumWD_Basic/src/test/java/HomeWorks/SeleniumTests.java
@@ -3,6 +3,7 @@ package HomeWorks;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -24,5 +25,12 @@ public class SeleniumTests {
         driver.findElement(By.xpath("//input[@id='username']")).sendKeys("tomsmith");
         driver.findElement(By.xpath("//input[@id='password']")).sendKeys("SuperSecretPassword!");
         driver.findElement(By.xpath("//button[@class='radius']")).click();
+    }
+
+    @AfterMethod
+    public void closeDriver (){
+       if(driver != null) {
+           driver.quit();
+       }
     }
 }
