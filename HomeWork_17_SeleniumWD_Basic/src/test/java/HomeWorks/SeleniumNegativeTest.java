1b904bf840f7c7dfbd09d07f721863dd10a21656
krasstas <supremewizard81@gmail.com>
2024-03-20 19:58:55 +0200

    Separate tests.

--- HomeWork_17_SeleniumWD_Basic/src/test/java/HomeWorks/SeleniumNegativeTest.java
+++ HomeWork_17_SeleniumWD_Basic/src/test/java/HomeWorks/SeleniumNegativeTest.java
@@ -1,13 +1,15 @@
 package HomeWorks;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class SeleniumTests {
+public class SeleniumNegativeTest {
 
     protected WebDriver driver;
     private static final By MESSAGE = By.xpath("//div[@id='flash']");
@@ -22,15 +24,24 @@ public class SeleniumTests {
     public void KrasTest() {
         driver.get("https://the-internet.herokuapp.com/login");
 
-        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("tomsmith");
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.xpath("//button[@class='radius']")).click();
+        driver.findElement(By.xpath("//input[@id='username']"))
+                .sendKeys("IncorrectUserName");
+        driver.findElement(By.xpath("//input[@id='password']"))
+                .sendKeys("IncorrectPassword!");
+        driver.findElement(By.xpath("//button[@class='radius']"))
+                .click();
+
+        WebElement el = driver.findElement(By.xpath("//div[@id='flash']"));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String str = js.executeScript("return arguments[0].firstChild.textContent", el).toString();
+        System.out.println("The message is: " + str);
     }
 
     @AfterMethod
     public void closeDriver (){
-       if(driver != null) {
-           driver.quit();
-       }
+        if(driver != null) {
+            driver.quit();
+        }
     }
 }
---
+++ HomeWork_17_SeleniumWD_Basic/src/test/java/HomeWorks/SeleniumPositiveTest.java
@@ -0,0 +1,44 @@
+package HomeWorks;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SeleniumPositiveTest {
+
+    protected WebDriver driver;
+    private static final By MESSAGE = By.xpath("//div[@id='flash']");
+    private static final String ALERT_BASE_LOCATOR = "//button[@onclick='%s']";
+    private final String LOGIN_PAGE_URL = "https://the-internet.herokuapp.com/login";
+    @BeforeMethod
+    public void initBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void KrasTest() {
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("tomsmith");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//button[@class='radius']")).click();
+
+        WebElement el = driver.findElement(By.xpath("//div[@id='flash']"));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String str = js.executeScript("return arguments[0].firstChild.textContent", el).toString();
+        System.out.println("The message is: " + str);
+    }
+
+    @AfterMethod
+    public void closeDriver (){
+       if(driver != null) {
+           driver.quit();
+       }
+    }
+}
