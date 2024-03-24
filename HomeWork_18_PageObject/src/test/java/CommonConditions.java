021f9ec394db973b92692000d152fb0a9ccac243
krasstas <supremewizard81@gmail.com>
2024-03-24 13:05:07 +0200

    Precondition for HW_18.

---
+++ HomeWork_18_PageObject/src/test/java/CommonConditions.java
@@ -0,0 +1,26 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+public class CommonConditions {
+    protected WebDriver driver;
+    protected static final Logger logger = (Logger) LogManager.getLogger(CommonConditions.class);
+    protected static final By MESSAGE = By.xpath("//div[@id='flash']");
+    protected static final String ALERT_BASE_LOCATOR = "//button[@onclick='%s']";
+    protected static final String LOGIN_PAGE_URL = "https://the-internet.herokuapp.com/login";
+    @BeforeMethod
+    public void initBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void closeDriver (){
+        if(driver != null) {
+            driver.quit();
+        }
+    }
+}
